f7649e46366f2a11440406f98380a781756ee428
Mark Mumo <marcmumo@gmail.com>
2018-07-25 20:37:13 +0300

    add addtendance

--- src/students/admin.py
+++ src/students/admin.py
@@ -6,8 +6,10 @@ from .models import Student
 from .models import Department
 from .models import Course
 from .models import Lecturers
+from .models import Attendence
 
 admin.site.register(Student)
 admin.site.register(Department)
 admin.site.register(Course)
 admin.site.register(Lecturers)
+admin.site.register(Attendence)
--- src/students/models.py
+++ src/students/models.py
@@ -1,5 +1,7 @@
 import uuid
 from django.db import models
+from datetime import datetime
+from django.utils import timezone
 
 # Create your models here. 
 
@@ -38,10 +40,9 @@ class Course(models.Model):
     unit_name = models.CharField(max_length=200, null=False)
     year = models.CharField(max_length=200, choices=COURSE_YEAR_CHOICES, default='First_Year_First_Semester')
     department = models.ForeignKey(Department, related_name="courses",on_delete=models.PROTECT)
-    # lecturer = models.ForeignKey(Lecturers, related_name="units_taught",on_delete=models.PROTECT)
 
-    def __unicode__(self):
-        return u"{} ({} cfu)".format(self.unit_name, self.unit_code)
+    def __str__(self):
+        return u'%s %s %s' % (self.unit_code, self.unit_name, self.department)
 
 class Lecturers(models.Model):
     first_name = models.CharField(max_length=255, null=False, blank=False)
@@ -49,14 +50,8 @@ class Lecturers(models.Model):
     other_names = models.CharField(max_length=255, null=True, blank=True)
     department = models.ForeignKey(Department, related_name="lecturer_courses",on_delete=models.PROTECT)
 
-    def get_full_name(self):
-        return '{} {}'.format(self.first_name, self.last_name)
-
-    def get_short_name(self):
-        return self.first_name
-
     def __str__(self):
-        return self.first_name
+        return u'%s %s %s' % (self.first_name, self.last_name, self.department)
 
 class Student(models.Model):
    #guid = models.UUIDField(unique=True, default=uuid.uuid4)
@@ -65,7 +60,6 @@ class Student(models.Model):
     other_names = models.CharField(max_length=255, null=True, blank=True)
     registration_number = models.CharField(unique=True, max_length=20, null=True, blank=False)
     department = models.ForeignKey(Department, related_name="student_course",on_delete=models.PROTECT)
-    # year = models.ForeignKey(Course, choices=COURSE_YEAR_CHOICES, on_delete=models.PROTECT)
     id_no_Passport = models.CharField(max_length=255, null=False, blank=False)
     country = models.CharField(max_length=255, null=True, blank=True)
     date_of_birth = models.DateField(blank=False, null=False)
@@ -88,8 +82,27 @@ class Student(models.Model):
     def get_full_name(self):
         return '{} {}'.format(self.first_name, self.last_name)
 
-    def get_short_name(self):
-        return self.first_name
+    def __str__(self):
+        return u'%s %s %s %s %s' % (self.first_name, self.last_name, self.registration_number, self.department, self.image)
+
+class Attendence(models.Model):
+    
+    ATTENDANCE_TYPES = (
+        ('present', 'Present'),
+        ('absent', 'Absent'),
+        ('excused', 'Excused'),
+    )
+    registration_number = models.ForeignKey(Student,related_name="stud_reg",on_delete=models.PROTECT)
+    department = models.ForeignKey(Department, related_name="course_att",on_delete=models.PROTECT)
+    unit_name = models.ForeignKey(Course, related_name="unit_att",on_delete=models.PROTECT)
+    start_time = models.DateTimeField(null=True, blank=True)
+    end_time = models.DateTimeField(null=True, blank=True)
+    duration = models.DecimalField(max_digits=5, decimal_places=2, default=0)
+    date = models.DateField(default=timezone.now)
+    status = models.CharField(max_length=10, choices=ATTENDANCE_TYPES, default='')
+    absent_with_reason = models.BooleanField(default=False)
+    detail = models.TextField(default='no')
+
 
     def __str__(self):
-        return self.registration_number
+        return u'%s %s %s %s %s %s' % (self.registration_number, self.department, self.unit_name, self.date, self.unit_name, self.image)
